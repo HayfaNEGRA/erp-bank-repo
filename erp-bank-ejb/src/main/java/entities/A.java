2415f245b2e0ed8d53df8e0d0645e1fa394d186b
Hayfa <Hayfa@172.16.159.91>
2015-10-16 10:33:12 +0100

    Signed-off-by: Hayfa <Hayfa@172.16.159.91>

--- erp-bank-ejb/src/main/java/entities/A.java
+++ erp-bank-ejb/src/main/java/entities/A.java
@@ -1,25 +1,25 @@
 package entities;
 
 import java.io.Serializable;
-import java.lang.Integer;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * Entity implementation class for Entity: A
  *
  */
 @Entity
-
 public class A implements Serializable {
 
-	
 	private Integer id;
 	private static final long serialVersionUID = 1L;
 
 	public A() {
 		super();
-	}   
-	@Id    
+	}
+
+	@Id
 	public Integer getId() {
 		return this.id;
 	}
@@ -27,5 +27,8 @@ public class A implements Serializable {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-   
+
+	public void ahla() {
+
+	}
 }
